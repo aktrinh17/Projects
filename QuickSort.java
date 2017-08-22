0b09aea8cfc6187f242fbbca48a3615866b9eb62
aktrinh17 <aktrinh@bu.edu>
2017-08-22 00:22:23 -0400

    Added code to initialize array variables and to generate a randomized array

--- QuickSort.java
+++ QuickSort.java
@@ -2,41 +2,43 @@
 import java.util.Arrays;
 
 public class QuickSort
-   {
+{
+   private static int[] anArray;
+   private static int arrayLength; 
    
    public static void main(String[] args)
-      {
+   {
+      QuickSort hArray = new QuickSort(10);
+      hArray.createRandArray();
+      System.out.println(Arrays.toString(QuickSort.anArray));
+   }
       
-      }
+   QuickSort(int currArraySize) 
+   {
+      arrayLength = currArraySize;
+      anArray = new int[arrayLength];
+   }
+    
+   public void createRandArray() //method to create an array with random values from 10 to 100 
+   {
+      for (int i = 0; i < arrayLength; i++)
+      {
+         anArray[i] = (int)(Math.random() * 91) + 10;
+      } 
+   }
    
-   QuickSort() 
-      {
+   public void switchVals()
+   {
       
-      }
-      
-   Partitioner()
-      {
-      
-      }
-      
+   }
+    
    public void quickSort()
-      {
+   {
       
-      }
+   }
       
    public void partition()
-      {
+   {
       
-      }
-      
-   public void switchVals()
-      {
-      
-      }
-      
-   public void createRandArray()
-      {
-      
-      }
-
-   }
+   }            
+}
