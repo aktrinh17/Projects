72005d933264dbe1b1d5b9dec5ee53f041f1559a
aktrinh17 <aktrinh@bu.edu>
2017-08-23 22:25:12 -0400

    Added skeleton for the code

---
+++ MergeSort.java
@@ -0,0 +1,27 @@
+//MergeSort
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MergeSort
+{
+
+   public static void main(String[] args)
+   {
+   
+   }
+   
+   public void createRandArray()
+   {
+   
+   }
+   
+   public static int divide()
+   {
+   
+   }
+   
+   public static int merge()
+   {
+   
+   }
+}
