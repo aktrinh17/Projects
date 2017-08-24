8ca5f46b465e59f4ed2c904bcf4dbc48da5e0856
aktrinh17 <aktrinh@bu.edu>
2017-08-23 23:19:55 -0400

    Added divide method

--- MergeSort.java
+++ MergeSort.java
@@ -37,9 +37,62 @@ public class MergeSort
          } 
    }
    
-   public void divide()
+   //method to divide an array into individual arrays
+   //containing only 1 element each
+   public static int divide(int[] otherArray)
    {
-   
+      int[] result = new int[otherArray.length];
+      
+      //if the array length is 1, then the division
+      //of the arrays are complete, otherwise continue
+      if (otherArray.length <= 1)
+      {
+         return otherArray;
+      }
+      
+      //finds the middle of the array and defines 
+      // the left array 
+      int middle = otherArray.length/2;
+      int[] leftArray = new int[middle];
+         
+      //if the array is even, the right array can also
+      //be the same size as the left array 
+      int[] rightArray;
+      if (otherArray.length %2 == 0)
+      {
+         rightArray = new int[middle];
+      }
+      
+      //if the array is odd, the right array will
+      //gain an additional element 
+      else
+      {
+         rightArray = new int[++midpoint];
+      }
+      
+      //fills up the left array with the leftmost
+      //contents of the other array
+      for (int i = 0; i < middle; i++)
+      {
+         leftArray[i] = otherArray[i];
+      }
+      
+      //fills up the right array with the rightmost
+      //contents of the other array starting at the middle
+      int x = 0;
+      for (int n = middle; n < otherArray.length; n++)
+      {
+         rightArray[x++] = otherArray[n];
+      }
+      
+      //recursively divides the left and right portions
+      //of the array
+      leftArray = merge(leftArray);
+      rightArray = merge(rightArray);
+      
+      result = merge(leftArray, rightArray);
+      return result;
+
    }
    
    public void merge()
