64e995cde64695eea9bc19b660bfe3fb7471b7cf
aktrinh17 <aktrinh@bu.edu>
2018-02-20 11:33:08 -0500

    Added to the vector creation function

--- tttanalyzer.cpp
+++ tttanalyzer.cpp
@@ -1,14 +1,29 @@
+// Copyright 2018 Alexander Trinh aktrinh@bu.edu
 #include <iostream>
 #include <string>
-using std::string;
-using std::cerr;
+#include <array>
+#include <vector>
+#include <map>
+
+
 using std::cout;
-using std::cin;
+using std::string;
+using std::vector;
+
+int xWins(string);
+int yWins(string);
+int numMoves(string, char);
 
 char tttresult(string tttboard, bool * perror)
 {
 	// checks for invalid boards
-
+	if (tttboard.length() != 9)
+		*perror = 1;
+	for (int i = 0; i <= 9; i++)
+	{
+		if (tttboard[i] != '#' && tttboard[i] != 'x' && tttboard[i] != 'o')
+			*perror = 1;
+	}
 	// checks for more than one line of winning
 	if (xWins(tttboard) > 1)
 		return 'i';
@@ -106,7 +121,92 @@ int numMoves(string tttboard, char choice)
 	return count;
 }
 
-int main()
+//converts a string of numbers from a base to another base
+string convertbase(const string& numstr, const int frombase, const int tobase) {
+  string newBase = "";
+  int distance = 0;
+  int baseMulti = 1;
+  // loops from the end of the string to the beginning
+  for (int i = numstr.length() - 1; i >= 0; i--) {
+    distance = distance + (numstr[i] - '0') * baseMulti;
+    baseMulti = baseMulti * frombase;
+  }
+
+  while (distance > 0) {
+    string pad = "";
+    int addVar = (distance % tobase) + '0';
+    distance = distance / tobase;
+    pad = pad + (static_cast<char>(addVar));
+    newBase.insert(0, pad);
+  }
+
+  return newBase;
+}
+
+char tttresult(vector<Move> board) {
+  return 'i';
+}
+
+string charReplace(string orig, string remove, string change)
 {
-	return 0;
+	int position;
+	do
+	{
+		position = orig.find(remove);
+		if (position != -1)
+			orig.replace(position, remove.length(), change))
+	} while (position != -1);
+
+	return orig;
+}
+
+vector<string> get_all_boards() {
+  vector<string> boards;
+  string tttcombo;
+
+  // converts the decimal number into base 3
+  // then converts the base 3 number into a ttt format
+  // and adds it to the vector boards
+  for (int i = 0; i <= 19682; i++)
+  {
+  	tttcombo = convertbase(i, 10, 3);
+  	charReplace(tttcombo, '0', '#');
+  	charReplace(tttcombo, '1', 'x');
+  	charReplace(tttcombo, '2', 'o');
+  	boards.push_back(tttcombo);
+  }
+
+  return boards;
+}
+
+int main() {
+  int n;
+  std::string board;
+  Move m;
+  std::vector<Move> moves;
+  std::vector<std::string> boards;
+  std::string asktype;
+
+  while (std::cin >> asktype) {
+    if (asktype == "v") {  // test tttresult vector
+      moves.clear();
+      std::cin >> n;
+      for (int i = 0; i < n; i++) {
+        std::cin >> m.r >> m.c >> m.player;
+        moves.push_back(m);
+      }
+      std::cout << tttresult(moves) << "\n";
+    } else if (asktype == "s") {  // test tttresult string
+      std::cin >> board;
+      std::cout << tttresult(board) << "\n";
+    } else if (asktype == "a") {  // test get_all_boards
+      boards = get_all_boards();
+      for (auto b : boards) {
+        std::cout << b << "\n";
+      }
+    } else {
+      return 0;
+    }
+  }
+  return 0;
 }
