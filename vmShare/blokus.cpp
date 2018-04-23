2e2b3803b259505f204d84e4861bf44f0a8ab76f
aktrinh17 <aktrinh@bu.edu>
2018-04-22 20:22:13 -0400

    Added creating pieces and setting board size

--- vmShare/blokus.cpp
+++ vmShare/blokus.cpp
@@ -1,25 +1,30 @@
 // Copyright 2018 Alexander Trinh aktrinh@bu.edu
+// Copyright 2018 Zach Halvorson zthalv@bu.edu
 #include <iostream>
 #include <string>
+#include <map>
 #include <vector>
-#include <unordered_map>
-#include <utility>
 #include <algorithm>
+#include <utility>
+#include <unordered_map>
+
+// tuple, utility, set, unordered_map, unordered_set,
 
 using std::cin;
 using std::cout;
 using std::string;
 using std::vector;
 using std::unordered_map;
+using std::map;
 
 /* Blokus command glossary
 
 Notes:
- - the first tile is numbered 100, and each new 
+ - the first tile is numbered 100, and each new
    one is assigned the next available integer
- 
+
  - the upper left corner is 0,0
- 
+
  - first index is row, second index is column
 
 
@@ -104,53 +109,202 @@ RESIZE: make the board bigger or smaller. When smaller,
 
 typedef int TileID;
 
-class Tile {
 
-  // common interface. required.
- public:
-  void show() const;  // print out tile in tilebox format
-  void rotate();
-  void flipud();
-  void fliplr();
+int increment = 0;
+
+class Tile {
+  public:
+    int ID = 100;
+    int tileNum;
+    static vector<Tile*> tiles;
+    void show() const;  // print out tile in tilebox format
+    void rotate();
+    void flipud();
+    void fliplr();
 };
 
+//unordered_map<int, *Tile> tileBank;
 
 class Blokus {
-  int tileID = 100;
-  unordered_map<int, vector<string>> tileBank;
-  // common interface. required.
- 
- // collection of Tiles
+  public:
 
- public:
-  Tile* find_tile(TileID);
-  void create_piece() {
-    // read in strings
-    // make a Tile
-    // store it in a collection of Tiles
-  }
-  
-  void reset();
-  void show_tiles() const;
-  void show_board() const;
-  void play_tile(TileID, int, int);
-  void set_size(int);
+    Blokus() {
+
+    }
+
+    std::map<int, vector<string>> duplicates;
+    // std::map<int, vector<string>> tilebank;
+    unordered_map<int, Tile> tileBank;
+    vector<string> board;
+    // int Tile::id = 100;
+    int name = 100;
+    Tile data;
+    Tile* find_tile(TileID);
+    void create_piece();
+    void reset();
+    void show_tiles() const;
+    void show_board() const;
+    void play_tile(TileID, int, int);
+    void set_size(int);
 };
 
-void Blokus::create_piece() {
-  int boardSize;
-  cin >> boardSize;
-
-  vector<string> tiles;
-  for (int i = 0; i < boardSize; i++)
-  {
-    string input;
-    cin >> input;
-    tiles.push_back(input);
-  }
-  tileBank.emplace(tileID, tiles);
-  tileID++;
+// default constructor
+Tile::Tile() {
+  tileNum = ID;
+  ID++;
+  Tiles.push_back(this);
 }
+
+// constructor
+Tile::Tile(vector<string> completeTile) {
+  tileNum = ID;
+  ID++;
+  vector<string> tileLayoutData = completeTile;
+}
+
+void Tile::show() const{
+cout << "test";
+//  cout << TileID << "\n";
+//  for (loop through all of the strings of the vector for that id)
+//  cout <<
+}
+
+void Tile::rotate() {
+
+}
+
+void Tile::flipud() {
+
+}
+
+void Tile::fliplr() {
+
+}
+
+/*Tile* Blokus::find_tile(TileID input) {
+
+//this should be the main priority as the rotate, flip, and show functions all rely on being able to generate a Tile*
+//to the correct instance of the tile that is being requested
+
+  return &SOMETHING;
+}*/
+
+void Blokus::show_tiles() const {
+  cout << "tile inventory \n"; //tile inventory is a line that is required by his examples to be printed
+ // for (auto tile : tilebank)
+ //tile->show();
+/* print it out in the following format with the TileID first, then the tile
+102
+*
+101
+**
+**
+*/
+}
+
+void Blokus::show_board() const {
+//this should be easier to figure out as it is called b.show_board()
+
+//so here we could just access the vector of strings directly and loop through each string and print
+//it row by row
+
+}
+
+void Blokus::play_tile(TileID, int, int) {
+
+
+}
+
+void Blokus::reset(){
+  //need to clear all instances of the Tile class
+  duplicates.clear();
+  board.clear();
+  set_size(0);// might be unnecassry depending on how the clear command works for board.clear()
+  name = 100;//intended to reset the value of name in the blokus class to 100
+}
+
+void Blokus::set_size(int newsize) {//board is a vector of strings stored in the Blokus class that represents the board
+  if (board.size() == 0) { //checks if the board exists/is size 0
+    string row; //creates a string that represents the row by appending a certain length of this default string
+    row.append("......................................",newsize); //where newsize is the size of the board
+    for (int i = 0; i <= newsize; i++) {
+      board.push_back(row); //pushes back this string that is newsize characters long until the vector has newsize number of elements
+    }
+  } else {
+    int current = board.size();
+    int grow = newsize - current; //will only work for making the board larger
+    for (string& row : board) { //loops through the existing board and adds on periods to extend the board horizontally
+      row.append("........................", grow);
+    }
+    for (int i = current; i < newsize; i++) { //this adds on the rows at the bottom where the board didn't exist to extend it vertically downwards
+      string row;
+      row.append("......................................",newsize);
+      board.push_back(row);
+    }
+  }
+}
+
+void Blokus::create_piece() {
+  int tilesize;
+  cin >> tilesize;
+
+  int index = tilesize; //this term index correpsonds to the first column that contains a *, we set it ot the max size first and then reduce it
+  //as we check each row inputted
+
+  string filler;
+  filler.append("........................", tilesize);
+
+  vector<string> rough;
+  vector<string> polished;
+
+  for (int i = 0; i < tilesize; i++) { //each time this loop occurs it takes in the next row from CIN
+    string row;
+    cin >> row;
+    if (row.size() != tilesize)
+      //throw invalid if the row isn't the right size
+    if (row.find("*")!=std::string::npos) { //this checks that there is a * in the row, and pushes it back into a vector if it does, otherwise it ignores that row
+    //this is intended to make sure that the tile is as far up as possible, but there could be issues if there is an empty row in the middle of creating a tile
+    //would only be an issue when the user is trying to make two tiles in one command, as any blank lines are ignored and are not saved
+      rough.push_back(row);
+      if (row.find("*") < index) //determines the first column that contains a * by checking if the location of the first * in the row is less than the index
+      //the index starts at the end of the board, and is shifted left/decreased if the location of the first * is less than the location of all previous *s
+        index = row.find("*");
+    }
+  }
+
+  //the following occurs after the entire tile has been read in from CIN
+
+  for (auto i : rough) { //this function takes the rough vector representing the tile
+  //and trims off any empty columns to ensure that the tile is as far left as possible
+    string clipped = i.substr(index);
+    clipped.append("....................",index); //this then appends decimal points to the end of
+    //each row to ensure that the tile is of proper width, we haven't corrected the height yet
+    polished.push_back(clipped);
+  }
+  while (polished.size() != tilesize) { //this ensures that the tile is of the proper height by making
+  //sure that the vector has the same number of elements as the tilesize
+    polished.push_back(filler); //pushes back a row of decimal points of the proper width until the tile is now square
+  }
+
+  //here I would make the 8 alternate orientations and store those in a seperate map based on the weight of the tile to error check for duplicates
+
+  //Next we need to figure out how to create an instance of the Tile class and then assign the data to that instance.
+  /*Tile objectname;
+  objectname.tilebank = polished;*/
+  //need to figure out how to change the name of the Tile class each time
+
+  //this used to be a map<int vector<string>> where I would emplace the vector into the position correpsonding to the ID
+  //tilebank.emplace(ID, polished);
+
+  /*
+            // creates a unique tile object
+            Tile tileName_increment(ID, polished); 
+            // stores the tile in a map
+            tileBank.insert(std::make_pair(ID, tileName_increment));
+  */
+
+}
+
 
 // MAIN. Do not change the below.
 
