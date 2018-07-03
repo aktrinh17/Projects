0a51654d95376fdfb835898b2ee302bea02f1280
aktrinh17 <aktrinh@bu.edu>
2018-07-02 20:11:10 -0400

    Added Mongo databases for saving players

--- Bracketing/server.js
+++ Bracketing/server.js
@@ -2,44 +2,73 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
-//var bodyParser = require("body-parser");
+var bodyParser = require("body-parser");
 var app = express();
-var mongodb = require("mongodb");
+var mongoClient = require("mongodb").mongoClient;
+
 var staticFiles = express.static(__dirname + "/");
 app.use(staticFiles);
+//middleware to allow client data to be saved to database
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 var countedClicks = 0;
+
 var db;
 var databaseUrl = "mongodb://studentmrcode:mrcode123@ds135963.mlab.com:3"
+var mongoose = require("mongoose");
+//connects to database on default port 27017
+mongoose.Promise = global.Promise;mongoose.connect("mongodb://localhost:27017/my_db");
+var playerSchema = new mongoose.Schema({
+	name: String,
+	elo: Number
+});
+var Player = mongoose.model("Player", playerSchema);
+
+//stores player info to database
+app.post("/addPlayer", (req, res) => {
+	var playerData = new Player(req.body);
+	playerData.save()
+	.then(item => {
+		res.send("player saved to database");
+	})
+	.catch(err => {
+		res.status(400).send("unable to save to database");
+	});
+});
 
 app.set('view engine', 'ejs');
 
-/*mongodb.MongoClient.connect(databaseUrl, function(error, database) {
-	db = database;
+//logs player submissions with middleware function
+app.use("/createBracket", function(req, res, next){
+	console.log("A new challenjour has arrived: " + Date.now());
+	next();
+});
 
-	db.collection("records").findOne({
-		"_id": "clicks"
-	}, function(error, result) {
-		clicks = result.clicks;
-	});
-});*/
+app.get("/createBracket/:name/:elo([0-9]{4})", function(req, res) {
+	res.send("Name: " + req.params.name + " elo: " + req.params.elo);
+});
 
-/*app.use(bodyParser.urlencoded( {
-	extended : false
-}));
-app.use(bodyParser.json());*/
-
-app.post("/clicks", function (request, response) {
+app.post("/clicks", function(req, res){
 	countedClicks++;
-	response.render("index", {clicks: countedClicks});
+	res.render("index", {clicks: countedClicks});
 });
 
-app.get("/clicks", function (request, response) {
-	response.send(clicks + "");
+app.get("/clicks", function(req, res){
+	res.send(clicks + "");
 });
 
-app.get("/", function (request, response) {
-	response.render("index", {clicks: countedClicks});
+app.get('/players', function(req, res){
+	res.render('players');
+});
+
+app.get("/", function(req, res){
+	res.render("index", {clicks: countedClicks});
+});
+
+//handles invalid elo submissions
+app.get("*", function(req, res){
+	res.send("Please enter a valid elo");
 });
 
 app.listen(3000);
